8ece5aee87904fad37b7a41528227b8a4e847524
TPOTVIZ <shripatia302@gmail.com>
2022-03-07 08:29:15 +0530

    Projects commit

---
+++ portfolio-site/src/components/Projects.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Projects = ()=>{
+    return (
+            <div className= "max-w-4xl mx-auto justify-center py-12">
+                    <div className = "flex flex-wrap justify-center pt-2">
+                        
+                    </div>
+            
+            </div>
+
+
+
+    )
+}
